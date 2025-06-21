e62b578b5e6ea6b1f9f18d0af9db09a8d32648af
Grayson <grayson.campbell@gmail.com>
2025-06-21 21:08:12 +1000

    The big cleanup

--- src/pages/api/catalogs/sources/index.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import { promises as fs } from 'fs';
-
-export default async function handler(req, res) {
-  // Read the "data.json" file
-  const fileContents = await fs.readFile(process.cwd() + '/src/data/catalogs/sources.json', 'utf8');
-  // Return the content of the data file in JSON format
-  res.status(200).json(JSON.parse(fileContents));
-}
